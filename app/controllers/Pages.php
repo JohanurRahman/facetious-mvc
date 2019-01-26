46298aa16dd21cd203bb94029dee8a572db40a52
JohanurRahman <johanur.rahman@gmail.com>
2019-01-26 20:19:39 +0600

    Base Controller Created

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,18 +1,20 @@
 <?php
 
-    class Pages {
+    class Pages extends Controller {
+
         public function __construct() {
 
         }
         
         public function index() {
-            
+            $this->view('hello');
         }
 
         public function about($id) {
             // echo 'Its about method';
             echo $id;
         }
+        
     }
 
 

dfcc054582b775501524a1be7aa900e19adcb4c2
JohanurRahman <johanur.rahman@gmail.com>
2019-01-27 06:45:33 +0600

    Config file added

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+    # APP Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    # URL Root
+    define('URLROOT', 'http://localhost/facetious-mvc');
+
+    # Site Name
+    define('SITENAME', 'FacetiousMVC');
+
+
+?>
